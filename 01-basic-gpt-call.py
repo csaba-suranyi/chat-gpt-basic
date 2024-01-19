65ff6665d4e42873f678b0c6082e8580887538bc
csaba-suranyi <suranyi.csaba@protonmail.com>
2024-01-19 12:24:32 +0100

    api key, dotenv, os

--- 01-basic-gpt-call.py
+++ 01-basic-gpt-call.py
@@ -1,6 +1,10 @@
 import openai
+import os
+from dotenv import load_dotenv
 
-openai.api_key = "API KEY HERE"
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 model_name = "gpt-3.5-turbo"
 
